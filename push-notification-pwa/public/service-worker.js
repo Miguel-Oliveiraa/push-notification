c3abd12a750793957690d2017ea65b3af6b3d069
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-10-09 08:52:40 -0300

    oneSignal notifications

--- push-notification-pwa/public/service-worker.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-restricted-globals */
-self.addEventListener('push', function (event) {
-    const body = event.data?.text() ?? ''
-    event.waitUntil(
-        self.registration.showNotification('Push Notification', {
-            body
-        })
-    );
-});
--- push-notification-pwa/src/App.js
+++ push-notification-pwa/src/App.js
@@ -3,34 +3,46 @@ import './App.css';
 import { api } from './services/axios';
 
 const handlePushNotification = async () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
-      .then(async serviceWorker => {
-        let subscription = await serviceWorker.pushManager.getSubscription();
+  //change sw.js to the name of your previous service worker file
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+    window.OneSignalDeferred.push(async function(OneSignal) {
+        await OneSignal.init({
+            appId: "ee27e749-e300-4965-b13c-6512fada7e83",
+            notifyButton: {
+                enable: true
+            },
+            serviceWorkerParam: { scope: "/" },
+            serviceWorkerPath: "/OneSignalSDKWorker.js",
+        });
+    });
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.register('service-worker.js')
+  //     .then(async serviceWorker => {
+  //       let subscription = await serviceWorker.pushManager.getSubscription();
 
-        if (!subscription) {
-          const publicKeyResponse = await api.get('/push/publicKey');
+  //       if (!subscription) {
+  //         const publicKeyResponse = await api.get('/push/publicKey');
 
-          subscription = await serviceWorker.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: publicKeyResponse.data
-          });
-        }
+  //         subscription = await serviceWorker.pushManager.subscribe({
+  //           userVisibleOnly: true,
+  //           applicationServerKey: publicKeyResponse.data
+  //         });
+  //       }
 
-        await api.post('/push/subscribe', {
-          subscription,
-        })
+  //       await api.post('/push/subscribe', {
+  //         subscription,
+  //       })
 
-        await api.post('/push/sendNotification', {
-          subscription,
-        })
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
-  } else {
-    console.error('Service Workers are not supported in this browser.');
-  }
+  //       await api.post('/push/sendNotification', {
+  //         subscription,
+  //       })
+  //     })
+  //     .catch(error => {
+  //       console.error('Service Worker registration failed:', error);
+  //     });
+  // } else {
+  //   console.error('Service Workers are not supported in this browser.');
+  // }
 }
 
 
