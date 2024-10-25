266b6f91563bf2ea9ab2829da12065f94b7d6c74
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-10-25 14:02:06 -0300

    testee

--- push-notification-backend/index.js
+++ push-notification-backend/index.js
@@ -1,65 +1,71 @@
-const express = require('express');
-const webPush = require('web-push');
-const fs = require('fs');
-const https = require('https');
-const cors = require('cors');  
-require('dotenv').config();
+const express = require("express");
+const webPush = require("web-push");
+const fs = require("fs");
+const https = require("https");
+const cors = require("cors");
+require("dotenv").config();
 
 const port = process.env.PORT || 3001; // Use a variável de ambiente PORT
-const domain = process.env.APP_URL || '0.0.0.0'
-console.log(domain)
+const domain = process.env.APP_URL || "0.0.0.0";
+console.log(domain);
 
 // Chaves VAPID
 const vapidKeys = webPush.generateVAPIDKeys();
 
-webPush.setVapidDetails(`https://${domain}`, vapidKeys.publicKey, vapidKeys.privateKey);
+webPush.setVapidDetails(
+  `https://${domain}`,
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
 
 const app = express();
 
 // CORS
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
 });
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/push/publicKey', (req, res) => {
-    res.send(vapidKeys.publicKey);
+app.get("/push/publicKey", (req, res) => {
+  res.send(vapidKeys.publicKey);
 });
 
-app.post('/push/subscribe', (req, res) => {
-    const subscription = req.body;
-    console.log(subscription);
-    res.status(201).json({});
+app.post("/push/subscribe", (req, res) => {
+  const subscription = req.body;
+  console.log(subscription);
+  res.status(201).json({});
 });
 
-app.post('/push/sendNotification', async (req, res) => {
-    const { subscription }= req.body;
+app.post("/push/sendNotification", async (req, res) => {
+  const { subscription } = req.body;
 
-    const payload = JSON.stringify({ title: 'Push Test' });
-    setTimeout(()=>{
-        webPush.sendNotification(subscription, payload)
-            .then(() => {
-                res.status(201).json({});
-            })
-            .catch((error) => {
-                console.error(error);
-                res.status(500).json({});
-            });
-    }, 5000)
-   
+  const payload = JSON.stringify({ title: "Push Test" });
+  setTimeout(() => {
+    webPush
+      .sendNotification(subscription, payload)
+      .then(() => {
+        res.status(201).json({});
+      })
+      .catch((error) => {
+        console.log(subscription);
+        console.log(payload);
+        console.error(error);
+        res.status(500).json({});
+      });
+  }, 5000);
 });
 
 // Inicializar o servidor HTTPS
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Servidor HTTPS rodando em http:0.0.0.0:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Servidor HTTPS rodando em http:0.0.0.0:${port}`);
 });
--- push-notification-pwa/src/App.js
+++ push-notification-pwa/src/App.js
@@ -1,55 +1,55 @@
 "use client";
-import OneSignal from 'react-onesignal';
-import logo from './logo.svg';
-import './App.css';
-import { api } from './services/axios';
+import OneSignal from "react-onesignal";
+import logo from "./logo.svg";
+import "./App.css";
+import { api } from "./services/axios";
 
 function App() {
   // OneSignal initialization
-  const subscribeToOneSignal = () => {
-    // Ensure this code runs only on the client side
-    if (typeof window !== 'undefined') {
-      OneSignal.init({
-        appId: 'ee27e749-e300-4965-b13c-6512fada7e83',
-        // You can add other initialization options here
-        notifyButton: {
-          enable: true,
-        },
-        // Uncomment the below line to run on localhost. See: https://documentation.onesignal.com/docs/local-testing
-        // allowLocalhostAsSecureOrigin: true
-      });
-    }
-  }
-  
+  // const subscribeToOneSignal = () => {
+  //   // Ensure this code runs only on the client side
+  //   if (typeof window !== "undefined") {
+  //     OneSignal.init({
+  //       appId: "ee27e749-e300-4965-b13c-6512fada7e83",
+  //       // You can add other initialization options here
+  //       notifyButton: {
+  //         enable: true,
+  //       },
+  //       // Uncomment the below line to run on localhost. See: https://documentation.onesignal.com/docs/local-testing
+  //       // allowLocalhostAsSecureOrigin: true
+  //     });
+  //   }
+  // };
+
   // nodejs initizalition
   const subscribeToNodejsExpress = async () => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('service-worker.js')
-        .then(async serviceWorker => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("service-worker.js")
+        .then(async (serviceWorker) => {
           let subscription = await serviceWorker.pushManager.getSubscription();
           if (!subscription) {
-            const publicKeyResponse = await api.get('/push/publicKey');
+            const publicKeyResponse = await api.get("/push/publicKey");
             subscription = await serviceWorker.pushManager.subscribe({
               userVisibleOnly: true,
-              applicationServerKey: publicKeyResponse.data
+              applicationServerKey: publicKeyResponse.data,
             });
           }
-          await api.post('/push/subscribe', {
+          // await api.post("/push/subscribe", {
+          //   subscription,
+          // });
+          console.log(subscription);
+          await api.post("/push/sendNotification", {
             subscription,
-          })
-          await api.post('/push/sendNotification', {
-            subscription,
-          })
+          });
         })
-        .catch(error => {
-          console.error('Service Worker registration failed:', error);
+        .catch((error) => {
+          console.error("Service Worker registration failed:", error);
         });
     } else {
-      console.error('Service Workers are not supported in this browser.');
+      console.error("Service Workers are not supported in this browser.");
     }
-  }
-
-
+  };
 
   return (
     <div className="App">
@@ -58,8 +58,20 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={()=>{subscribeToOneSignal()}}>nodejs</button>
-        <button onClick={()=>{subscribeToNodejsExpress()}}>ondeSignal</button>
+        {/* <button
+          onClick={() => {
+            subscribeToOneSignal();
+          }}
+        >
+          nodejs
+        </button> */}
+        <button
+          onClick={() => {
+            subscribeToNodejsExpress();
+          }}
+        >
+          nodeJS
+        </button>
       </header>
     </div>
   );
